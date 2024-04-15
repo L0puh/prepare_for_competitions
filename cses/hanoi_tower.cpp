d649d5cd27c6779a193a57642c2bdb2f2b0b58e3
L0puh <1opuhart@gmail.com>
2024-04-15 20:43:34 +0900

    cses

--- cses/hanoi_tower.cpp
+++ cses/hanoi_tower.cpp
@@ -1,11 +1,26 @@
 #include <bits/stdc++.h>
 
+void move(int a, int b){
+   printf("%d %d\n", a, b);
+}
+
+void print_moves(int disks, int from, int mid, int to){
+   if (disks == 1) { 
+      move(from, to);
+      return;
+   }
+   print_moves(disks-1, from, to, mid); 
+   move(from, to);
+   print_moves(disks-1, mid, from, to);
+}
+
 int main(){
    int n;
    scanf("%d", &n);
    long min = (1<<n) - 1;
+
    printf("%ld\n", min);
-   //TODO: moves solution
+   print_moves(n, 1, 2, 3);
 
    return 0;
 }
